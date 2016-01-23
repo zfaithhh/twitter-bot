cf7a168d58c84843006f89c80457e5d6885474d0
Faith Zellman <zfaithhh@yahoo.com>
2016-01-22 20:41:22 -0500

    Splicing

--- app.js
+++ app.js
@@ -19,11 +19,21 @@ var newTweets = [
   "@roselpadilla: If at first you don't succeed, call it version 1.0.",
   "@roselpadilla: Are you sitting on the F5 key? Because your ass is refreshing!",
   "@roselpadilla: You had me at 'Hello World'"
+  "@roselpadilla: Yeezy for President 2020!",
+  "@roselpadilla: If you have the opportunity to play this game of life you need to appreciate every moment. a lot of people don't appreciate the moment until it's passed. - Kanye West",
+  "@roselpadilla: Get off your butt and do something.",
+  "@roselpadilla: There’s lots of bad reasons to start a company. But there’s only one good, legitimate reason, and I think you know what it is: it’s to change the world. - Phil Libin, CEO of Evernote.",
+  "@roselpadilla: https://www.youtube.com/watch?v=nk5SRhpitDI",
+  "@roselpadilla: The best thing about a boolean is even if you are wrong, you're only off by a bit.",
+  "@roselpadilla: The trouble with programmers is that you can never tell what a programmer is doing until its too late.",
+  "@roselpadilla: http://watchitvideos.com/ex-machina-2015-free-online36-putlocker2/",
+  "@roselpadilla: You're girlfriend loves you very very very very much.",
 ];
 
 function autoTweet() {
    var rand = newTweets[Math.floor(Math.random() * newTweets.length)];
    Bot.tweet(rand);
+   newTweets.splice(rand);
  }
 
 autoTweet();
